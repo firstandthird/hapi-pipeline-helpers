596c93f4a9056c1c9e5add31620168c25ddb56d6
Greg Allen <me@jga.me>
2016-02-25 17:34:56 -0800

    added sass support

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   styles: {
     less: require('./lib/less'),
     clean: require('./lib/css-clean'),
-    autoprefix: require('./lib/autoprefix')
+    autoprefix: require('./lib/autoprefix'),
+    sass: require('./lib/sass')
   },
   scripts: {
     include: require('./lib/js-include'),
